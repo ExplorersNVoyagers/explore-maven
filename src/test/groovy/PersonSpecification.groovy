53f823ea6dabf707b42407d382e5304fb40c0259
phaniv <phanikrishna@outlook.com>
2021-09-11 14:19:53 -0500

    test

--- src/test/groovy/PersonSpecification.groovy
+++ src/test/groovy/PersonSpecification.groovy
@@ -3,6 +3,21 @@ import spock.lang.Specification
 
 class PersonSpecification extends Specification {
 
+    def setupSpec() {
+        System.out.println("setupSpec");
+    }
+
+    def setup() {
+        System.out.println("setup");
+    }
+    def cleanup() {
+        System.out.println("cleanup");
+    }
+
+    def cleanupSpec() {
+        System.out.println("cleanupSpec");
+    }
+
     def "Should throw exception if first name or last name is empty"() {
         given:
         def person = new Person();
@@ -11,11 +26,12 @@ class PersonSpecification extends Specification {
         String fullName = person.getFullName(firstName, lastName);
 
         then:
+        System.out.println("setupSpec");
         thrown(IllegalArgumentException);
 
         where:
-        firstName << [null, "", " ","A","A","A",null, "", " "];
-        lastName << [null, "", " ",null, "", " ","A","A","A"];
+        firstName << [null, "", " ", "A", "A", "A", null, "", " "];
+        lastName << [null, "", " ", null, "", " ", "A", "A", "A"];
     }
 
     def "Should return full name if both first name and last name are not empty"() {
